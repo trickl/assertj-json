62fe73b79f765c929631f133265a94f5c9e881da
Tim Gee <writetotimgee@gmail.com>
2022-12-07 19:52:36 +0000

    Update GPG key

--- .travis/before-script.sh
+++ .travis/before-script.sh
@@ -1,6 +1,6 @@
 
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_39114eac217a_key -iv $encrypted_39114eac217a_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_fc29b8d9ac49_key -iv $encrypted_fc29b8d9ac49_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     gpg --fast-import .travis/codesigning.asc
 fi
